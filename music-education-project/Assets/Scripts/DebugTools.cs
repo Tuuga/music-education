7448b34c983158e2d2f31a13c689bcca1f89a94d
Tuuga-PC\Tuuga <tuukka.o.muukkonen@gmail.com>
2016-09-08 22:59:38 +0300

    UnitySynth

---
+++ music-education-project/Assets/Scripts/DebugTools.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugTools : MonoBehaviour {
+
+	public static void PrintArray(byte[] bytes) {
+		foreach (byte b in bytes) {
+			print(b);
+		}
+	}
+}
---
+++ music-education-project/Assets/Scripts/MidiReader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MidiReader : MonoBehaviour {
+	
+	public string path;
+
+	byte[] _rawMidiData;
+	public List<byte> _midiBytes;
+
+	void Awake () {
+		_rawMidiData = File.ReadAllBytes(path);
+		_midiBytes = new List<byte>(_rawMidiData);
+
+		File.WriteAllBytes("Assets/Resources/Midis/Song1text.txt", _rawMidiData);
+
+		print (CheckMidiTag(_rawMidiData));
+	}
+	
+	bool CheckMidiTag (byte[] bytes) {
+		string checker = "MThd";
+		string bytesString = "";
+		for (int i = 0; i < 4; i++) {
+			bytesString += (char)bytes[i];
+		}
+		if (bytesString == checker) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
